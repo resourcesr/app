93b3ad4a92a7f6996260e9a5eb33ed88d6ca534a
lablnet <mumerfarooqlablnet01@gmail.com>
2020-10-03 18:41:18 +0500

    fix drawer issue

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -3,7 +3,6 @@ import 'package:riphahwebresources/data/User.dart';
 import 'package:riphahwebresources/pages/Home/home_ui.dart';
 import 'package:riphahwebresources/pages/auth/login_ui.dart';
 import 'package:riphahwebresources/pages/auth/profile_ui.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 
 class WebResourceAppDrawer extends StatefulWidget {
@@ -31,61 +30,6 @@ class _AppDrawer extends State<AppDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> children = [];
-    children.add(
-      ListTile(
-        leading: Icon(Icons.home),
-        title: Text("Home"),
-        onTap: () => {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => HomeUi()))
-        },
-      ),
-    );
-    if (user.uid != null) {
-      children.add(ListTile(
-        leading: Icon(Icons.people),
-        title: Text("Profile"),
-        onTap: () => {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => ProfileUi(
-                        uid: user.uid,
-                      )))
-        },
-      ));
-      children.add(ListTile(
-        leading: Icon(Icons.timer_outlined),
-        title: Text("Timetable"),
-        onTap: () => {
-          // Navigator.push(
-          //context, MaterialPageRoute(builder: (context) => LoginUi()))
-        },
-      ));
-      children.add(ListTile(
-        leading: Icon(Icons.logout),
-        title: Text("Logout"),
-        onTap: () => {user.logout()},
-      ));
-    } else {
-      children.add(ListTile(
-        leading: Icon(Icons.people),
-        title: Text("Account"),
-        onTap: () => {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => LoginUi()))
-        },
-      ));
-    }
-    children.add(ListTile(
-      leading: Icon(Icons.settings),
-      title: Text("Settings"),
-      onTap: () => {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => LoginUi()))
-      },
-    ));
     /* return Drawer(
       child: ListView(
         padding: const EdgeInsets.all(8),
@@ -104,9 +48,53 @@ class _AppDrawer extends State<AppDrawer> {
             padding: const EdgeInsets.all(8),
             children: <Widget>[
               DrawerHeader(
-                child: Text("User ${user.uid}"),
+                child: Text("Mmenu"),
               ),
-              ...children,
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeUi()))
+                },
+              ),
+              if (user.loggedIn)
+                ListTile(
+                  leading: Icon(Icons.people),
+                  title: Text("Profile"),
+                  onTap: () => {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileUi(
+                                  uid: user.uid,
+                                )))
+                  },
+                ),
+              if (user.loggedIn)
+                ListTile(
+                  leading: Icon(Icons.timer_outlined),
+                  title: Text("Timetable"),
+                  onTap: () => {
+                    // Navigator.push(
+                    //context, MaterialPageRoute(builder: (context) => LoginUi()))
+                  },
+                ),
+              if (user.loggedIn)
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text("Logout"),
+                  onTap: () => {user.logout()},
+                ),
+              if (!user.loggedIn)
+                ListTile(
+                  leading: Icon(Icons.people),
+                  title: Text("Account"),
+                  onTap: () => {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginUi()))
+                  },
+                )
             ],
           ),
         ),
