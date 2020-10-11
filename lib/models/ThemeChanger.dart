ed7416269d4358fc452587b58060a0496cd9fa1f
lablnet <mumerfarooqlablnet01@gmail.com>
2020-10-11 14:43:22 +0500

    save preferece of theme state

--- lib/models/ThemeChanger.dart
+++ lib/models/ThemeChanger.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeChanger with ChangeNotifier {
   bool _isDark = false;
   bool get isDark => _isDark;
+
+  ThemeChanger() {
+    getState().then((value) => {_isDark = value ?? false});
+    notifyListeners();
+  }
+
+  Future<bool> getState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('theme');
+  }
+
+  void saveState(status) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("theme", status);
+  }
 
   ThemeMode currentTheme() {
     return _isDark ? ThemeMode.dark : ThemeMode.light;
@@ -10,6 +26,7 @@ class ThemeChanger with ChangeNotifier {
 
   void switchTheme() {
     _isDark = _isDark ? false : true;
+    saveState(isDark);
     notifyListeners();
   }
 }
