72269d95524af75c7f47d89cb946adcad55eade1
lablnet <mumerfarooqlablnet01@gmail.com>
2020-10-17 17:08:59 +0500

    added show password support

--- lib/components/custom_input.dart
+++ lib/components/custom_input.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomInput extends StatelessWidget {
+class CustomInput extends StatefulWidget {
   CustomInput(
       {this.validator,
       this.controller,
@@ -15,22 +15,40 @@ class CustomInput extends StatelessWidget {
   final int maxLines;
 
   @override
+  _CustomInputState createState() => _CustomInputState();
+}
+
+class _CustomInputState extends State<CustomInput> {
+  bool obscurePass = true;
+
+  IconButton _getSufixIcon(bool obscureText) {
+    if (!obscureText) return null;
+    return IconButton(
+      icon: obscurePass ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+      onPressed: () => {
+        setState(() => {obscurePass = !obscurePass})
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
         child: Padding(
       padding: const EdgeInsets.all(20.20),
       child: TextFormField(
-        obscureText: obscureText,
+        obscureText: widget.obscureText ? obscurePass : false,
         //initialValue: initialValue,
-        controller: controller,
-        validator: validator,
-        maxLines: maxLines ?? 1,
+        controller: widget.controller,
+        validator: widget.validator,
+        maxLines: widget.maxLines ?? 1,
         decoration: InputDecoration(
           border: OutlineInputBorder(
               borderSide: BorderSide(color: Theme.of(context).accentColor)),
           focusedBorder: OutlineInputBorder(
               borderSide: BorderSide(color: Theme.of(context).accentColor)),
-          labelText: label,
+          labelText: widget.label,
+          suffixIcon: _getSufixIcon(widget.obscureText),
         ),
       ),
     ));
